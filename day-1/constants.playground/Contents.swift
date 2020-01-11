d2049e6a6ff72cca74f2d23491fcbf525598913c
Prabhu Bose <pbose@pandora.com>
2020-01-11 12:30:57 -0800

    Wrapped up day 2 activities

--- day-1/constants.playground/Contents.swift
+++ day-1/constants.playground/Contents.swift
@@ -6,4 +6,6 @@ let paul = "Paul McCartney"
 let george = "George Harrison"
 let ringo = "Ringo Starr"
 
-let beatles = [john, paul, george, ringo]ss
+let beatles = [john, paul, george, ringo]
+
+let second = beatles[1]
